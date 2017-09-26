b24e773cded2e9531d1a129be62a350c0cc6a900
Lidia Gianne <lidiagianne@gmail.com>
2017-09-25 23:39:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var app = require('./config/server');
 
-app.listen(3000, function(req, res){
-	console.log("Servidor ON");
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
+/*app.listen(3000, function(req, res){
+	console.log("Servidor ON");
+});*/
